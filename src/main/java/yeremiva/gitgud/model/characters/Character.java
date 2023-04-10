f41d4d6e6c86c11c4e61d75b06bbdce4e06dd076
Ivan Yeremenko <yeremiva@fel.cvut.cz>
2023-04-11 01:14:45 +0200

    Added the Player class and refactored some code

---
+++ src/main/java/yeremiva/gitgud/model/characters/Character.java
@@ -0,0 +1,9 @@
+package yeremiva.gitgud.model.characters;
+
+public abstract class Character {
+    protected float x, y;
+    public Character(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+}
