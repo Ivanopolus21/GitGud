ec4b67867b8d5ab09477633a611b4863b6e1c4ed
Ivan Yeremenko <yeremiva@fel.cvut.cz>
2023-04-25 13:14:06 +0200

    Some refactoring of the Skeleton code

--- src/main/java/yeremiva/gitgud/model/characters/Enemy.java
+++ src/main/java/yeremiva/gitgud/model/characters/Enemy.java
@@ -9,14 +9,14 @@ import static yeremiva.gitgud.core.settings.Constants.EnemyConstants.*;
 import static yeremiva.gitgud.core.settings.HelpMethods.*;
 
 public abstract class Enemy extends Character{
-    private int aniIndex, enemyState, enemyType;
-    private int aniTick, aniSpeed = 25;
-    private boolean firstUpdate = true;
-    private boolean inAir;
-    private float fallSpeed;
-    private float gravity = 0.04f * GameController.SCALE;
-    private float walkSpeed = 0.5f * GameController.SCALE;
-    private int walkDir = LEFT;
+    protected int aniIndex, enemyState, enemyType;
+    protected int aniTick, aniSpeed = 25;
+    protected boolean firstUpdate = true;
+    protected boolean inAir;
+    protected float fallSpeed;
+    protected float gravity = 0.04f * GameController.SCALE;
+    protected float walkSpeed = 0.5f * GameController.SCALE;
+    protected int walkDir = LEFT;
     public Enemy(float x, float y, int width, int height, int enemyType) {
         super(x, y, width, height);
         this.enemyType = enemyType;
@@ -29,7 +29,41 @@ public abstract class Enemy extends Character{
         g.drawRect((int) hitbox.x - xLvlOffset, (int) hitbox.y, (int) hitbox.width, (int) hitbox.height);
     }
 
-    private void updateAnimationTick() {
+    protected void firstUpdateCheck(int[][] lvlData) {
+        if (!IsCharacterOnFloor(hitbox, lvlData)) {
+            inAir = true;
+        }
+        firstUpdate = false;
+    }
+
+    protected void updateInAir(int[][] lvlData) {
+        if (CanMoveHere(hitbox.x, hitbox.y + fallSpeed, hitbox.width, hitbox.height, lvlData)) {
+            hitbox.y += fallSpeed;
+            fallSpeed += gravity;
+        } else {
+            inAir = false;
+            hitbox.y = GetCharacterYPosUnderRoofOrAboveFloor(hitbox, fallSpeed);
+        }
+    }
+
+    protected void move(int[][] lvlData) {
+        float xSpeed = 0;
+
+        if (walkDir == LEFT) {
+            xSpeed = -walkSpeed;
+        } else {
+            xSpeed = walkSpeed;
+        }
+        if (CanMoveHere(hitbox.x + xSpeed, hitbox.y, hitbox.width, hitbox.height, lvlData)) {
+            if (IsFloor(hitbox, xSpeed, lvlData)) {
+                hitbox.x += xSpeed;
+                return;
+            }
+        }
+        changeWalkDir();
+    }
+
+    protected void updateAnimationTick() {
         aniTick++;
         if (aniTick >= aniSpeed){
             aniTick = 0;
@@ -40,52 +74,7 @@ public abstract class Enemy extends Character{
         }
     }
 
-    public void update(int[][] lvlData) {
-        updateMove(lvlData);
-        updateAnimationTick();
-    }
-
-    private void updateMove(int[][] lvlData) {
-        if (firstUpdate) {
-            if (!IsCharacterOnFloor(hitbox, lvlData)) {
-                inAir = true;
-            }
-            firstUpdate = false;
-        }
-        if (inAir) {
-            if (CanMoveHere(hitbox.x, hitbox.y + fallSpeed, hitbox.width, hitbox.height, lvlData)) {
-                hitbox.y += fallSpeed;
-                fallSpeed += gravity;
-            } else {
-                inAir = false;
-                hitbox.y = GetCharacterYPosUnderRoofOrAboveFloor(hitbox, fallSpeed);
-            }
-        } else {
-            switch(enemyState) {
-                case IDLE:
-                    enemyState = RUNNING;
-                    break;
-                case RUNNING:
-                    float xSpeed = 0;
-
-                    if (walkDir == LEFT) {
-                        xSpeed = -walkSpeed;
-                    } else {
-                        xSpeed = walkSpeed;
-                    }
-                    if (CanMoveHere(hitbox.x + xSpeed, hitbox.y, hitbox.width, hitbox.height, lvlData)) {
-                        if (IsFloor(hitbox, xSpeed, lvlData)) {
-                            hitbox.x += xSpeed;
-                            return;
-                        }
-                    }
-                    changeWalkDir();
-                    break;
-            }
-        }
-    }
-
-    private void changeWalkDir() {
+    protected void changeWalkDir() {
         if (walkDir == LEFT) {
             walkDir = RIGHT;
         } else {
--- src/main/java/yeremiva/gitgud/model/characters/Skeleton.java
+++ src/main/java/yeremiva/gitgud/model/characters/Skeleton.java
@@ -4,12 +4,38 @@ import yeremiva.gitgud.controller.GameController;
 
 import java.awt.*;
 
+import static yeremiva.gitgud.core.settings.Constants.Directions.LEFT;
 import static yeremiva.gitgud.core.settings.Constants.EnemyConstants.*;
+import static yeremiva.gitgud.core.settings.HelpMethods.*;
 
 public class Skeleton extends Enemy{
     public Skeleton(float x, float y) {
         super(x, y, SKELETON_WIDTH, SKELETON_HEIGHT, SKELETON);
         initHitbox(x, y, (int) (21 * GameController.SCALE), (int) (31 * GameController.SCALE));
+    }
+
+    public void update(int[][] lvlData) {
+        updateMove(lvlData);
+        updateAnimationTick();
+    }
+
+
+    private void updateMove(int[][] lvlData) {
+        if (firstUpdate) {
+            firstUpdateCheck(lvlData);
+        }
+        if (inAir) {
+            updateInAir(lvlData);
+        } else {
+            switch(enemyState) {
+                case IDLE:
+                    enemyState = RUNNING;
+                    break;
+                case RUNNING:
+                    move(lvlData);
+                    break;
+            }
+        }
     }
 
 }
