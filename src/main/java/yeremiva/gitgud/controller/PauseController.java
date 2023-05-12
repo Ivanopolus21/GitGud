b27440ad81a619c516e82bbaf6ec85bb93ebd393
Ivan Yeremenko <yeremiva@fel.cvut.cz>
2023-05-12 12:51:53 +0200

    Separation of Pause Controller and Pause View

--- src/main/java/yeremiva/gitgud/controller/PauseController.java
+++ src/main/java/yeremiva/gitgud/controller/PauseController.java
@@ -2,10 +2,7 @@ package yeremiva.gitgud.controller;
 
 import yeremiva.gitgud.core.settings.LoadSave;
 import yeremiva.gitgud.core.states.Gamestate;
-import yeremiva.gitgud.view.PauseButton;
-import yeremiva.gitgud.view.SoundButton;
-import yeremiva.gitgud.view.UrmButton;
-import yeremiva.gitgud.view.VolumeButton;
+import yeremiva.gitgud.view.*;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
@@ -18,53 +15,24 @@ import static yeremiva.gitgud.core.settings.Constants.View.VolumeButtons.*;
 //PAUSE OVERLAY
 public class PauseController {
     private GameProcessController gameProcessController;
-    private BufferedImage backgroundImg;
-    private int bgX, bgY, bgWidth, bgHeight;
+    private PauseView pauseView;
     private SoundButton musicButton, sfxButton;
     private UrmButton menuB, replayB, unpauseB;
     private VolumeButton volumeButton;
 
     public PauseController(GameProcessController gameProcessController){
         this.gameProcessController = gameProcessController;
-        loadBackground();
-        createSoundButtons();
-        createUrmButtons();
-        createVolumeButton();
+        this.pauseView = new PauseView(this);
+        this.musicButton = pauseView.getMusicButton();
+        this.sfxButton = pauseView.getSfxButton();
+        this.menuB = pauseView.getMenuB();
+        this.replayB = pauseView.getReplayB();
+        this.unpauseB = pauseView.getUnpauseB();
+        this.volumeButton = pauseView.getVolumeButton();
     }
 
-    private void createVolumeButton() {
-        int volumeX = (int) (309 * GameController.SCALE);
-        int volumeY = (int) (258 * GameController.SCALE);
-        volumeButton = new VolumeButton(volumeX, volumeY, SLIDER_WIDTH, VOLUME_HEIGHT);
-    }
-
-    //The method creates URM buttons (replay, menu, unpause)
-    private void createUrmButtons() {
-        int menuX = (int) (313 * GameController.SCALE);
-        int replayX = (int) (387 * GameController.SCALE);
-        int unpauseX = (int) (462 * GameController.SCALE);
-        int buttonY = (int) (305 * GameController.SCALE);
-
-        menuB = new UrmButton(menuX, buttonY, URM_SIZE, URM_SIZE, 2);
-        replayB = new UrmButton(replayX, buttonY, URM_SIZE, URM_SIZE, 1);
-        unpauseB = new UrmButton(unpauseX, buttonY, URM_SIZE, URM_SIZE, 0);
-    }
-
-    private void createSoundButtons() {
-        int soundX = (int) (450 * GameController.SCALE);
-        int musicY = (int) (115 * GameController.SCALE);
-        int sfxY = (int) (161 * GameController.SCALE);
-        musicButton = new SoundButton(soundX, musicY, SOUND_SIZE, SOUND_SIZE);
-        sfxButton = new SoundButton(soundX, sfxY, SOUND_SIZE, SOUND_SIZE);
-    }
-
-    //Function that loads background of the pause menu
-    private void loadBackground() {
-        backgroundImg = LoadSave.GetSpriteAtlas(LoadSave.PAUSE_BACKGROUND);
-        bgWidth = (int) (backgroundImg.getWidth() * GameController.SCALE);
-        bgHeight = (int) (backgroundImg.getHeight() * GameController.SCALE);
-        bgX = GameController.GAME_WIDTH / 2 - bgWidth / 2;
-        bgY = (int) (80 * GameController.SCALE);
+    public void draw(Graphics g) {
+        pauseView.draw(g);
     }
 
     public void update(){
@@ -76,27 +44,6 @@ public class PauseController {
         unpauseB.update();
 
         volumeButton.update();
-    }
-
-    public void draw(Graphics g){
-
-        g.setColor(new Color(0, 0, 0, 200));
-        g.fillRect(0, 0, GameController.GAME_WIDTH, GameController.GAME_HEIGHT);
-
-        //Background
-        g.drawImage(backgroundImg, bgX, bgY, bgWidth, bgHeight, null);
-
-        //Sound buttons
-        musicButton.draw(g);
-        sfxButton.draw(g);
-
-        //Urm buttons
-        menuB.draw(g);
-        replayB.draw(g);
-        unpauseB.draw(g);
-
-        //Volume slider
-        volumeButton.draw(g);
     }
 
     public void mouseDragged(MouseEvent e){
@@ -181,4 +128,5 @@ public class PauseController {
     public boolean isIn(MouseEvent e, PauseButton b) {
         return (b.getBounds().contains(e.getX(), e.getY()));
     }
+
 }
--- src/main/java/yeremiva/gitgud/view/PauseView.java
+++ src/main/java/yeremiva/gitgud/view/PauseView.java
@@ -1,5 +1,112 @@
 package yeremiva.gitgud.view;
 
+import yeremiva.gitgud.controller.GameController;
+import yeremiva.gitgud.controller.GameProcessController;
+import yeremiva.gitgud.controller.PauseController;
+import yeremiva.gitgud.core.settings.LoadSave;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import static yeremiva.gitgud.core.settings.Constants.View.PauseButtons.SOUND_SIZE;
+import static yeremiva.gitgud.core.settings.Constants.View.URMButtons.URM_SIZE;
+import static yeremiva.gitgud.core.settings.Constants.View.VolumeButtons.SLIDER_WIDTH;
+import static yeremiva.gitgud.core.settings.Constants.View.VolumeButtons.VOLUME_HEIGHT;
+
 public class PauseView {
+    private PauseController pauseController;
+    private BufferedImage backgroundImg;
+    private int bgX, bgY, bgWidth, bgHeight;
+    private SoundButton musicButton, sfxButton;
+    private UrmButton menuB, replayB, unpauseB;
+    private VolumeButton volumeButton;
+
+    public PauseView(PauseController pauseController) {
+        this.pauseController = pauseController;
+
+        loadBackground();
+        createSoundButtons();
+        createUrmButtons();
+        createVolumeButton();
+    }
+
+    private void createVolumeButton() {
+        int volumeX = (int) (309 * GameController.SCALE);
+        int volumeY = (int) (258 * GameController.SCALE);
+        volumeButton = new VolumeButton(volumeX, volumeY, SLIDER_WIDTH, VOLUME_HEIGHT);
+    }
+
+    //The method creates URM buttons (replay, menu, unpause)
+    private void createUrmButtons() {
+        int menuX = (int) (313 * GameController.SCALE);
+        int replayX = (int) (387 * GameController.SCALE);
+        int unpauseX = (int) (462 * GameController.SCALE);
+        int buttonY = (int) (305 * GameController.SCALE);
+
+        menuB = new UrmButton(menuX, buttonY, URM_SIZE, URM_SIZE, 2);
+        replayB = new UrmButton(replayX, buttonY, URM_SIZE, URM_SIZE, 1);
+        unpauseB = new UrmButton(unpauseX, buttonY, URM_SIZE, URM_SIZE, 0);
+    }
+
+    private void createSoundButtons() {
+        int soundX = (int) (450 * GameController.SCALE);
+        int musicY = (int) (115 * GameController.SCALE);
+        int sfxY = (int) (161 * GameController.SCALE);
+        musicButton = new SoundButton(soundX, musicY, SOUND_SIZE, SOUND_SIZE);
+        sfxButton = new SoundButton(soundX, sfxY, SOUND_SIZE, SOUND_SIZE);
+    }
+
+    //Function that loads background of the pause menu
+    private void loadBackground() {
+        backgroundImg = LoadSave.GetSpriteAtlas(LoadSave.PAUSE_BACKGROUND);
+        bgWidth = (int) (backgroundImg.getWidth() * GameController.SCALE);
+        bgHeight = (int) (backgroundImg.getHeight() * GameController.SCALE);
+        bgX = GameController.GAME_WIDTH / 2 - bgWidth / 2;
+        bgY = (int) (80 * GameController.SCALE);
+    }
+
+    public void draw(Graphics g){
+        g.setColor(new Color(0, 0, 0, 200));
+        g.fillRect(0, 0, GameController.GAME_WIDTH, GameController.GAME_HEIGHT);
+
+        //Background
+        g.drawImage(backgroundImg, bgX, bgY, bgWidth, bgHeight, null);
+
+        //Sound buttons
+        musicButton.draw(g);
+        sfxButton.draw(g);
+
+        //Urm buttons
+        menuB.draw(g);
+        replayB.draw(g);
+        unpauseB.draw(g);
+
+        //Volume slider
+        volumeButton.draw(g);
+    }
+
+    public SoundButton getMusicButton() {
+        return musicButton;
+    }
+
+    public SoundButton getSfxButton() {
+        return sfxButton;
+    }
+
+    public UrmButton getMenuB() {
+        return menuB;
+    }
+
+    public UrmButton getReplayB() {
+        return replayB;
+    }
+
+    public UrmButton getUnpauseB() {
+        return unpauseB;
+    }
+
+    public VolumeButton getVolumeButton() {
+        return volumeButton;
+    }
 
 }
