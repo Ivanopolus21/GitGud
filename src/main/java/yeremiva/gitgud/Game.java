76e91b7dbc8a5737892746876a1b92361ae6620f
Ivan Yeremenko <yeremiva@fel.cvut.cz>
2023-05-15 01:54:03 +0200

    Final refactoring of the main class

--- src/main/java/yeremiva/gitgud/Game.java
+++ src/main/java/yeremiva/gitgud/Game.java
@@ -1,10 +1,10 @@
 package yeremiva.gitgud;
 
 import yeremiva.gitgud.controller.GameController;
-import yeremiva.gitgud.view.GameWindowView;
-//MAIN CLASS
+
+
 public class Game {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new GameController();
     }
 }
