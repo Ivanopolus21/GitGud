a997b49dd0631e42b918cf464ef583a6bf7e908d
Ivan Yeremenko <yeremiva@fel.cvut.cz>
2023-05-15 02:00:40 +0200

    Final refactoring of the states

--- src/main/java/yeremiva/gitgud/core/states/Gamestate.java
+++ src/main/java/yeremiva/gitgud/core/states/Gamestate.java
@@ -1,7 +1,6 @@
 package yeremiva.gitgud.core.states;
 
 public enum Gamestate {
-
     PLAYING, MENU, OPTIONS, QUIT;
 
     public static Gamestate state = MENU;
--- src/main/java/yeremiva/gitgud/core/states/State.java
+++ src/main/java/yeremiva/gitgud/core/states/State.java
@@ -1,19 +1,19 @@
 package yeremiva.gitgud.core.states;
 
 import yeremiva.gitgud.controller.GameController;
-import yeremiva.gitgud.view.GameMenuButton;
+import yeremiva.gitgud.view.GameMenuButtonView;
 
 import java.awt.event.MouseEvent;
 
 public class State {
-
     protected GameController gameController;
+
     public State(GameController gameController) {
         this.gameController = gameController;
     }
 
     //Function that says if player pressing inside the button
-    public boolean isIn(MouseEvent e, GameMenuButton gmb) {
+    public boolean isIn(MouseEvent e, GameMenuButtonView gmb) {
         return gmb.getBounds().contains(e.getX(), e.getY());
     }
 
