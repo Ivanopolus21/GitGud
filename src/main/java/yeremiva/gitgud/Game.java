f923558640d851ef215f3e7090dadc5c520dc25f
Ivan Yeremenko <yeremiva@fel.cvut.cz>
2023-05-18 01:45:48 +0200

    Added logger and JAVADoc comments for the Game class

--- src/main/java/yeremiva/gitgud/Game.java
+++ src/main/java/yeremiva/gitgud/Game.java
@@ -4,10 +4,19 @@ import yeremiva.gitgud.controller.GameController;
 
 import java.util.logging.Logger;
 
-
+/**
+ * Game class.
+ */
 public class Game {
     private final static Logger log = Logger.getLogger(Game.class.getName());
 
+    /**
+     * Main class.
+     * <p>
+     *     Main class of the project that starts the actual game.
+     * </p>
+     * @param args the args
+     */
     public static void main(String[] args) {
         new GameController();
 
